e743c5abd401a0729ca2cdf952452a61471e548c
Miranda Melina <mmiranda@yopapp.me>
2019-12-12 12:38:24 -0300

    add tableview

--- UITableView1/ViewController.swift
+++ UITableView1/ViewController.swift
@@ -8,13 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+struct Contact {
+       var name:String?
+       var surname:String?
+   }
+
+class ViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var namelabel: UILabel!
+    @IBOutlet weak var surnamelabel: UILabel!
+    
+    var contacts = [Contact]()
+   
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        contacts = [
+        Contact.init(name: "Melina", surname: "Miranda"),
+        Contact.init(name: "Melina", surname: "Miranda"),
+        Contact.init(name: "Melina", surname: "Miranda"),
+        Contact.init(name: "Melina", surname: "Miranda"),
+        Contact.init(name: "Melina", surname: "Miranda"),
+        Contact.init(name: "Melina", surname: "Miranda"),
+        Contact.init(name: "Melina", surname: "Miranda"),
+        Contact.init(name: "Melina", surname: "Miranda"),
+        Contact.init(name: "Melina", surname: "Miranda"),
+        Contact.init(name: "Melina", surname: "Miranda"),
+        
+        ]
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return contacts.count
     }
 
 
-}
+   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+    
+    cell.textLabel?.text  = contacts[indexPath.row].name
+    cell.detailTextLabel?.text  = contacts[indexPath.row].surname
+ 
+          return cell
+    }
+   
+ }
 
